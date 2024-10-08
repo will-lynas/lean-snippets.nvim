d071bcd2e243d1dd5d0e1d6c765e7c8b3155b4fa
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-10-08 14:39:36 +0100

    Add snippets

---
+++ lua/lean-snippets.lua
@@ -0,0 +1,38 @@
+local M = {}
+
+function M.setup()
+	local ls = require("luasnip")
+	local s = ls.snippet
+	local t = ls.text_node
+	local i = ls.insert_node
+	local r = require("luasnip.extras").rep
+
+	ls.add_snippets("lean", {
+		s("cs", {
+			t({ "constructor", "· " }),
+			i(1),
+			t({ "", "  sorry", "·", "  sorry" }),
+		}),
+
+		s("cor", {
+			t("cases "),
+			i(1),
+			t({ " with", "| inl h =>", "  sorry", "| inr h =>", "  sorry" }),
+		}),
+
+		s("ot", {
+			t("obtain ⟨ _, _ ⟩ := "),
+			i(1),
+		}),
+
+		s("nd", {
+			t("induction "),
+			i(1),
+			t({ " with", "| zero =>", "  sorry", "| succ " }),
+			r(1),
+			t({ " ih =>", "  sorry" }),
+		}),
+	})
+end
+
+return M
